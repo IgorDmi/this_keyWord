db3b3c8e093f17621bb869d384b6163e04be4940
IgorDmi <60873886+IgorDmi@users.noreply.github.com>
2021-02-23 13:55:56 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace this_keyWord
+{
+    class Aircraft
+    {
+        public string Manufacturer;
+        public string Model;
+
+        public Aircraft(string Manufacturer, string Model)
+        {
+            this.Manufacturer = Manufacturer;
+            this.Model = Model;
+        }
+
+        public string GetName()
+        {
+            return ($"{Manufacturer}, {Model}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aircraft plane1 = new Aircraft("Comac", "C919");
+
+            Console.WriteLine(plane1.GetName());
+        }
+    }
+}
